b062f5c8e3782ab8400dcb44b295398f9362a877
Sfsdfsd <fdfs@fff,com>
2017-11-19 19:24:06 +0100

    about agregado

--- Curso-Angular-Udemy-Portfolio/portafolio/src/app/app.module.ts
+++ Curso-Angular-Udemy-Portfolio/portafolio/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { HomeComponent } from './components/home/home.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
